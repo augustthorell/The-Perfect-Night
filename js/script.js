4e6ddd10de6b83d894a65dc3aaf925fd63648667
August Thorell <augustthorell@gmail.com>
2020-11-19 14:10:25 +0100

    added animation to hamburger-menu

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 
 function toggleNavBar() {
     mainNav.classList.toggle('active');
+    navBarToggle.classList.toggle('opened');
 }
-
 
 navBarToggle.addEventListener('click', toggleNavBar);
