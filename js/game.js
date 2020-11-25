e45ad6e48a791ff987bad28c22859c39389db92c
August Thorell <augustthorell@gmail.com>
2020-11-25 11:37:31 +0100

    Added modal for game and vibration

--- js/game.js
+++ js/game.js
@@ -1,15 +1,22 @@
-const startGameButton = document.getElementById('start-game');
+const startGameButton = document.getElementById("start-game");
+const questionBox = document.querySelector(".game-question");
+let modalBackground = document.querySelector(".input_div");
+let buttonTrue = document.getElementById("True");
+let buttonFalse = document.getElementById("False");
+
+let correctAnswer;
 
 function randomTimer() {
     getQuestion();
     // let randomTime = (Math.floor(Math.random * 120000) + 10000);
-    let randomTime = (Math.floor(Math.random() * 5000) + 1000);
+    let randomTime = Math.floor(Math.random() * 5000) + 1000;
     setTimeout(function() {
-        console.log('You lose, loser');
+        //console.log("You lose, loser");
     }, randomTime);
 }
 
 function getQuestion() {
+    modalBackground.style.background = "#364c59";
     fetch("https://opentdb.com/api.php?amount=1&difficulty=easy&type=boolean")
         .then(function(response) {
             if (response.status !== 200) {
@@ -20,8 +27,9 @@ function getQuestion() {
             }
 
             response.json().then(function(data) {
-
-                displayQuestion(data.results[0]);
+                //console.log(data);
+                displayQuestion(data.results[0].question);
+                correctAnswer = data.results[0].correct_answer;
             });
         })
         .catch(function(err) {
@@ -29,10 +37,42 @@ function getQuestion() {
         });
 }
 
-
 function displayQuestion(question) {
-    console.log(question.question);
-    console.log(question.correct_answer);
+    questionBox.innerHTML = question;
 }
 
-startGameButton.addEventListener('click', randomTimer);
+function checkAnswer() {
+    if (this.id == correctAnswer) {
+        console.log("You answered Correctly");
+        modalBackground.style.background = "#2a8c2f";
+    } else {
+        console.log("You answered Wrong");
+        modalBackground.style.background = "#a32428";
+        window.navigator.vibrate(500);
+    }
+    setTimeout(getQuestion, 3000);
+}
+
+startGameButton.addEventListener("click", randomTimer);
+buttonTrue.addEventListener("click", checkAnswer);
+buttonFalse.addEventListener("click", checkAnswer);
+
+// Popup
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("start-game");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+("");
